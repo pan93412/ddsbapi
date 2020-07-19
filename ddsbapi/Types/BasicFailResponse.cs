6d2ba3ffc2175411120fc0092552cd729b93eab6
pan93412 <pan93412@gmail.com>
2020-07-19 23:27:38 +0800

    feat(types/basicfailresp): the basic response whose Success is false.

---
+++ ddsbapi/Types/BasicFailResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ddsbapi.Types
+{
+    /// <summary>
+    /// The basic response whose <c>Success</c> is false.
+    /// </summary>
+    public class BasicFailResponse : IResponse
+    {
+        /// <inheritdoc/>
+        public bool Success => false;
+    }
+}
