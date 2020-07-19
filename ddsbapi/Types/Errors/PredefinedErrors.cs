ac8128710ef77bb49a4a90734a8838e026ff5b77
pan93412 <pan93412@gmail.com>
2020-07-19 23:29:37 +0800

    feat(types/PredefinedErrors): add predefined errors

---
+++ ddsbapi/Types/Errors/PredefinedErrors.cs
@@ -0,0 +1,20 @@
+﻿namespace ddsbapi.Types.Errors
+{
+    /// <summary>
+    /// The predefined errors.
+    /// </summary>
+    public static class PredefinedErrors
+    {
+        /// <summary>
+        /// BAD_URL: The URL to shorten is invaild.
+        /// </summary>
+        public static readonly ErrorResponse BadUrl = new ErrorResponse
+        {
+            Error =
+            {
+                Code = "BAD_URL",
+                Message = "The URL to shorten is invaild."
+            }
+        };
+    }
+}
