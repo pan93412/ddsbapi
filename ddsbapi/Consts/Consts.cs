b24f185c526ae833416ccd44537308f3d10294f2
pan93412 <pan93412@gmail.com>
2020-07-19 22:35:53 +0800

    feat(const): add NewShortenLink
    Generate a new random string whose length is 8 and only with digits and lowercase letters.

---
+++ ddsbapi/Consts/Consts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ddsbapi.Consts
+{
+    /// <summary>
+    /// Consts.
+    /// </summary>
+    public static class Consts
+    {
+        private const string randomSource = "abcdefghijklmnopqrstuvwxyz0123456789";
+        private static readonly Random random = new Random();
+
+        /// <summary>
+        /// Generate a new random string whose length is 8
+        /// and only with digits and lowercase letters.
+        /// </summary>
+        public static string NewShortenLink => new string(
+            Enumerable.Repeat(randomSource, 8)
+                .Select(s => s[random.Next(s.Length)])
+                .ToArray()
+        );
+    }
+}
