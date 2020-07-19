e3d09e3a9f19fc46785ed9ff9b6f4b3a2b342a73
pan93412 <pan93412@gmail.com>
2020-07-19 23:29:14 +0800

    feat(types/error): the response sent when something wrong

---
+++ ddsbapi/Types/Errors/ErrorDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace ddsbapi.Types.Errors
+{
+    /// <summary>
+    /// The details of this error.
+    /// </summary>
+    public class ErrorDetails
+    {
+        /// <summary>
+        /// The error code.
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// The corresponding error message.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ ddsbapi/Types/Errors/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ddsbapi.Types.Errors
+{
+    /// <summary>
+    /// The response when the request is bad.
+    /// </summary>
+    public class ErrorResponse : BasicFailResponse
+    {
+        /// <summary>
+        /// The error details.
+        /// </summary>
+        public ErrorDetails Error { get; set; }
+    }
+}
