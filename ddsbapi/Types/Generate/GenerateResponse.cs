0b8da8b70494eb6ebe728ad804ceaa32e10ebcaa
pan93412 <pan93412@gmail.com>
2020-07-20 00:47:04 +0800

    feat(types/GenerateResponse): the simplified Link class.

---
+++ ddsbapi/Types/Generate/GenerateResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Emit;
+
+namespace ddsbapi.Types.Generate
+{
+    /// <summary>
+    /// The response of <c>/generate</c>.
+    /// </summary>
+    public class GenerateResponse
+    {
+        /// <summary>
+        /// The ID of this shortened URL.
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// The password of this dd.sb URL.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
