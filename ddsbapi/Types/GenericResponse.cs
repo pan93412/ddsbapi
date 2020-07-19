dc2b1dff0500237dfc76873bbdf935e2b538f00c
pan93412 <pan93412@gmail.com>
2020-07-19 23:05:26 +0800

    feat(types/GenericResponse): add the generic response

---
+++ ddsbapi/Types/GenericResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ddsbapi.Types
+{
+    /// <summary>
+    /// The generic response.
+    /// </summary>
+    /// <typeparam name="TData">The type of <see cref="Data"/></typeparam>
+    public class GenericResponse<TData> : BasicOkResponse
+    {
+        /// <summary>
+        /// The data in <typeparamref name="TData"/>
+        /// </summary>
+        public TData Data { get; set; }
+    }
+}
