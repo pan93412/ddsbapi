296e5414942f6ce2e80591e6465e68284b3b448d
pan93412 <pan93412@gmail.com>
2020-07-19 22:52:54 +0800

    feat(types/generate): add the request section

---
+++ ddsbapi/Types/Generate/GenerateRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace ddsbapi.Types.Generate
+{
+    /// <summary>
+    /// The request for <c>/generate</c>.
+    /// </summary>
+    public class GenerateRequest
+    {
+        /// <summary>
+        /// The url to be shorten.
+        /// </summary>
+#pragma warning disable CA1056 // It is from the request.
+        public string Url { get; set; }
+#pragma warning restore CA1056 // It is from the request.
+        /// <summary>
+        /// The password of the shorten url.
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// The custom id for the shorten url.
+        /// </summary>
+        public string CustomId { get; set; }
+    }
+}
