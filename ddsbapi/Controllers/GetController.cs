09dfbea70483f7ec6a8935f0454f3b0409be922d
pan93412 <pan93412@gmail.com>
2020-07-20 19:50:23 +0800

    feat(controller/get): done

---
+++ ddsbapi/Controllers/GetController.cs
@@ -0,0 +1,58 @@
+﻿using ddsbapi.Model;
+using ddsbapi.Types;
+using ddsbapi.Types.Errors;
+using ddsbapi.Types.Get;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ddsbapi.Controllers
+{
+    /// <summary>
+    /// Get the long URL of the given id.
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class GetController : ControllerBase
+    {
+        private readonly DDSBContext ctx;
+        /// <summary>
+        /// The constructor of <see cref="GetController"/>
+        /// </summary>
+        /// <param name="ctx">The <see cref="DDSBContext"/></param>
+        public GetController(DDSBContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        /// <summary>
+        /// Get the long URL of the given <paramref name="linkId"/>
+        /// </summary>
+        /// <param name="linkId">The link ID.</param>
+        /// <param name="password">The password of this link.</param>
+        /// <returns>The original link.</returns>
+        [HttpGet("{linkId}")]
+        public ActionResult<GenericResponse<GetResponse>> GetLink(string linkId, [FromQuery] string password = null)
+        {
+            Link link = ctx.Link.Where(link =>
+                link.LinkBrowseId == linkId
+            ).FirstOrDefault();
+
+            if (link == null)
+            {
+                return BadRequest(PredefinedErrors.NoSuchUrl);
+            }
+            else if (link.Password != password)
+            {
+                return BadRequest(PredefinedErrors.WrongPassword);
+            }
+
+            return new GenericResponse<GetResponse>
+            {
+                Data = new GetResponse
+                {
+                    Url = link.RedirectURL.ToString()
+                }
+            };
+        }
+    }
+}
