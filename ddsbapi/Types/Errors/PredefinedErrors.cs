633263b5b1b46d69f44b8075816f821b9e20ebb2
pan93412 <pan93412@gmail.com>
2020-07-20 18:42:21 +0800

    feat(types/error): add BAD_URL, also fixup some object issues

--- ddsbapi/Types/Errors/PredefinedErrors.cs
+++ ddsbapi/Types/Errors/PredefinedErrors.cs
@@ -10,10 +10,22 @@
         /// </summary>
         public static readonly ErrorResponse BadUrl = new ErrorResponse
         {
-            Error =
+            Error = new ErrorDetails
             {
                 Code = "BAD_URL",
                 Message = "The URL to shorten is invaild."
+            }
+        };
+
+        /// <summary>
+        /// BAD_URL: The URL to shorten is invaild.
+        /// </summary>
+        public static readonly ErrorResponse IdHasBeenTaken = new ErrorResponse
+        {
+            Error = new ErrorDetails
+            {
+                Code = "ID_HAS_BEEN_TAKEN",
+                Message = "This custom ID has been taken."
             }
         };
     }
