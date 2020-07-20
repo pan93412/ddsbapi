b708aa54a8d138242c40782679bdf2c206ae4216
pan93412 <pan93412@gmail.com>
2020-07-20 19:50:12 +0800

    feat(types/GetResponse): the response of `/get`

---
+++ ddsbapi/Types/Get/GetResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace ddsbapi.Types.Get
+{
+    /// <summary>
+    /// The response of <c>/get</c>.
+    /// </summary>
+    public class GetResponse
+    {
+        /// <summary>
+        /// The URL you should redirect to.
+        /// </summary>
+#pragma warning disable CA1056
+        public string Url { get; set; }
+#pragma warning restore CA1056
+    }
+}
